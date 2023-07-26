0f0a6e374673a078ac3a7ef5d0026a0d6695e6aa
bcrone <crone@umich.edu>
2023-07-26 10:00:40 -0400

    Update SNP paths

--- single-tissue-model/run_PLINK_scores.sh
+++ single-tissue-model/run_PLINK_scores.sh
@@ -44,17 +44,17 @@ PLINK2_PATH="/nfs/turbo/boylelab/plink2/plink2"
 
 #SURF
 \${PLINK_PATH} --bfile \${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump \${SUMSTATS} --clump-snp-field SNP --clump-field P \
-        --extract \${SURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.GENERIC.\${SLURM_ARRAY_TASK_ID}.SNPs --out \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}.clump
+        --extract \${SURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.\${SLURM_ARRAY_TASK_ID}.GENERIC.SNPs --out \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}.clump
 
 \${PLINK2_PATH} --pgen \${BED_PATH} --pvar \${BIM_PATH} --psam \${FAM_PATH} --score \${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range \${RANGE_LIST} \${SUMSTATS}.SNP.pvalue \
         --extract \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}.clump.clumped --keep \${SAMPLES_PATH} --out \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}
 
 #TURF
 \${PLINK_PATH} --bfile \${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump \${SUMSTATS} --clump-snp-field SNP --clump-field P \
-        --extract \${TURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.\${TISSUE}.\${SLURM_ARRAY_TASK_ID}.SNPs --out \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}.clump
+        --extract \${TURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.\${SLURM_ARRAY_TASK_ID}.\${TISSUE}.SNPs --out \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}.clump
 
 \${PLINK2_PATH} --pgen \${BED_PATH} --pvar \${BIM_PATH} --psam \${FAM_PATH} --score \${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range \${RANGE_LIST} \${SUMSTATS}.SNP.pvalue \
-        --extract \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}.clump.clumped --keep \${SAMPLES_PATH} --out \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}
+        --extract \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}.clump.clumped --keep \${SAMPLES_PATH} --out \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}
 
 exit
 EOF
