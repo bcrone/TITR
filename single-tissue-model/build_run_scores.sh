820472a1d4c1b4f5d9cb9af5a025a0052994a738
bcrone <crone@umich.edu>
2023-07-27 10:13:04 -0400

    build_run_scores.sh - trait/ancestry jobs

--- single-tissue-model/build_run_scores.sh
+++ single-tissue-model/build_run_scores.sh
@@ -5,13 +5,13 @@ ANCESTRY="$2"
 TRAIT="$3"
 TISSUE="$4"
 
-rm slurm/run_calculate_PLINK_scores.jobs
-rm slurm/run_calculate_standard_scores.jobs
+rm slurm/${TRAIT}.${ANCESTRY}.run_calculate_PLINK_scores.jobs
+rm slurm/${TRAIT}.${ANCESTRY}.run_calculate_standard_scores.jobs
 
 bash run_standard_scores.sh ${ROOT_PATH} ${ANCESTRY} ${TRAIT} > slurm/${TRAIT}.${ANCESTRY}.run_standard_scores.sh
 bash run_PLINK_scores.sh ${ROOT_PATH} ${ANCESTRY} ${TRAIT} ${TISSUE} > slurm/${TRAIT}.${ANCESTRY}.run_PLINK_scores.sh
 
 RES=$(sbatch slurm/${TRAIT}.${ANCESTRY}.run_standard_scores.sh)
-echo "${RES##* }" >> slurm/run_standard_scores.jobs
+echo "${RES##* }" >> slurm/${TRAIT}.${ANCESTRY}.run_standard_scores.jobs
 RES=$(sbatch slurm/${TRAIT}.${ANCESTRY}.run_PLINK_scores.sh)
-echo "${RES##* }" >> slurm/run_PLINK_scores.jobs
+echo "${RES##* }" >> slurm/${TRAIT}.${ANCESTRY}.run_PLINK_scores.jobs
