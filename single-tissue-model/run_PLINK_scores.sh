f6797fb30b5dd3b76221a45ffc72b4cf4603b5bf
bcrone <crone@umich.edu>
2023-09-13 15:18:44 -0400

    Add run_PLINK_scores rule

--- single-tissue-model/run_PLINK_scores.sh
+++ single-tissue-model/run_PLINK_scores.sh
@@ -1,60 +1,52 @@
-#!/bin/bash
-cat <<EOF
-#!/bin/bash
-#SBATCH --job-name=$2.$3.run_PLINK_scores
-#SBATCH --cpus-per-task=1
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=65g
-#SBATCH --time=5:00:00
-#SBATCH --account=
-#SBATCH --partition=
-#SBATCH --output=logs/%x.log
-#SBATCH --array=10,50,100,200,500
-
 ROOT_PATH="$1"
-ANCESTRY="$2"
-TRAIT="$3"
-TISSUE="$4"
+RESULTS_PATH="$2"
+ANCESTRY="$3"
+TRAIT="$4"
+TISSUE_PATH="$5"
+PLINK_PATH="$6"
+PLINK2_PATH="$7"
 
-EUR_1KG_BFILE="\${ROOT_PATH}/data/1KG/1000G_EUR_Phase3_plink/1000G.EUR.QC"
-RANGE_LIST="\${ROOT_PATH}/data/UKB/phenos/range_list.expanded"
-SUMSTATS="\${ROOT_PATH}/data/GWAS/\${TRAIT}/\${TRAIT}.PLINK.TITR"
+TISSUE=`cat ${TISSUE_PATH}`
+PARTITIONS=( 10 50 100 200 500 )
 
-BED_PATH="\${ROOT_PATH}/data/UKB/\${ANCESTRY}/ukb_imp_chrALL_v3.bed"
-BIM_PATH="\${ROOT_PATH}/data/UKB/\${ANCESTRY}/ukb_imp_chrALL_v3.bim"
-FAM_PATH="\${ROOT_PATH}/data/UKB/phenos/\${ANCESTRY}/\${ANCESTRY}.\${TRAIT}.fam"
-SAMPLES_PATH="\${ROOT_PATH}/data/UKB/phenos/\${ANCESTRY}/\${ANCESTRY}.\${TRAIT}.sample.IDs"
+EUR_1KG_BFILE="${ROOT_PATH}/data/1KG/1000G_EUR_Phase3_plink/1000G.EUR.QC"
+SUMSTATS="${ROOT_PATH}/data/GWAS/${TRAIT}/${TRAIT}.PLINK.TITR"
+RANGE_LIST="${ROOT_PATH}/data/UKB/phenos/range_list.expanded"
 
-IMPACT_PATH="\${ROOT_PATH}/data/IMPACT/\${TRAIT}"
-SURF_PATH=\${ROOT_PATH}/data/RegDB/GENERIC
-TURF_PATH="\${ROOT_PATH}/data/RegDB/\${TISSUE}"
+BED_PATH="${ROOT_PATH}/data/UKB/${ANCESTRY}/ukb_imp_chrALL_v3.bed"
+BIM_PATH="${ROOT_PATH}/data/UKB/${ANCESTRY}/ukb_imp_chrALL_v3.bim"
+FAM_PATH="${ROOT_PATH}/data/UKB/phenos/${ANCESTRY}/${ANCESTRY}.${TRAIT}.fam"
+SAMPLES_PATH="${ROOT_PATH}/data/UKB/phenos/${ANCESTRY}/${ANCESTRY}.${TRAIT}.sample.IDs"
 
-RESULTS_PATH="\${ROOT_PATH}/results/single_tissue/\${TRAIT}"
+#Standard
+${PLINK_PATH} --bfile ${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump ${SUMSTATS} --clump-snp-field SNP --clump-field P 
+		--out ${RESULTS_PATH}/standard/${TRAIT}.${ANCESTRY}.standard.clump
 
-PLINK_PATH="/nfs/turbo/boylelab/plink/plink"
-PLINK2_PATH="/nfs/turbo/boylelab/plink2/plink2"
+${PLINK2_PATH} --pgen ${BED_PATH} --pvar ${BIM_PATH} --psam ${FAM_PATH} --score ${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range ${RANGE_LIST} ${SUMSTATS}.SNP.pvalue 
+        --extract ${RESULTS_PATH}/standard/${TRAIT}.${ANCESTRY}.standard.clump.clumped --keep ${SAMPLES_PATH} --out ${RESULTS_PATH}/standard/${TRAIT}.${ANCESTRY}.standard
 
-#IMPACT
-\${PLINK_PATH} --bfile \${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump \${SUMSTATS} --clump-snp-field SNP --clump-field P \
-        --extract \${IMPACT_PATH}/IMPACT707_EUR_chrALL.\${SLURM_ARRAY_TASK_ID}.\${TRAIT}.SNPs --out \${RESULTS_PATH}/IMPACT/\${TRAIT}.\${ANCESTRY}.IMPACT.\${SLURM_ARRAY_TASK_ID}.clump
+for PARTITION in ${PARTITIONS[@]}
+do
+	#IMPACT
+	${PLINK_PATH} --bfile ${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump ${SUMSTATS} --clump-snp-field SNP --clump-field P 
+	        --extract ${IMPACT_PATH}/IMPACT707_EUR_chrALL.${PARTITION}.${TRAIT}.SNPs --out ${RESULTS_PATH}/IMPACT/${TRAIT}.${ANCESTRY}.IMPACT.${PARTITION}.clump
 
-\${PLINK2_PATH} --pgen \${BED_PATH} --pvar \${BIM_PATH} --psam \${FAM_PATH} --score \${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range \${RANGE_LIST} \${SUMSTATS}.SNP.pvalue \
-        --extract \${RESULTS_PATH}/IMPACT/\${TRAIT}.\${ANCESTRY}.IMPACT.\${SLURM_ARRAY_TASK_ID}.clump.clumped --keep \${SAMPLES_PATH} --out \${RESULTS_PATH}/IMPACT/\${TRAIT}.\${ANCESTRY}.IMPACT.\${SLURM_ARRAY_TASK_ID}
+	${PLINK2_PATH} --pgen ${BED_PATH} --pvar ${BIM_PATH} --psam ${FAM_PATH} --score ${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range ${RANGE_LIST} ${SUMSTATS}.SNP.pvalue 
+	        --extract ${RESULTS_PATH}/IMPACT/${TRAIT}.${ANCESTRY}.IMPACT.${PARTITION}.clump.clumped --keep ${SAMPLES_PATH} --out ${RESULTS_PATH}/IMPACT/${TRAIT}.${ANCESTRY}.IMPACT.${PARTITION}
 
-#SURF
-\${PLINK_PATH} --bfile \${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump \${SUMSTATS} --clump-snp-field SNP --clump-field P \
-        --extract \${SURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.GENERIC.\${SLURM_ARRAY_TASK_ID}.SNPs --out \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}.clump
+	#SURF
+	${PLINK_PATH} --bfile ${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump ${SUMSTATS} --clump-snp-field SNP --clump-field P 
+	        --extract ${SURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.GENERIC.${PARTITION}.SNPs --out ${RESULTS_PATH}/SURF/${TRAIT}.${ANCESTRY}.SURF.${PARTITION}.clump
 
-\${PLINK2_PATH} --pgen \${BED_PATH} --pvar \${BIM_PATH} --psam \${FAM_PATH} --score \${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range \${RANGE_LIST} \${SUMSTATS}.SNP.pvalue \
-        --extract \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}.clump.clumped --keep \${SAMPLES_PATH} --out \${RESULTS_PATH}/SURF/\${TRAIT}.\${ANCESTRY}.SURF.\${SLURM_ARRAY_TASK_ID}
+	${PLINK2_PATH} --pgen ${BED_PATH} --pvar ${BIM_PATH} --psam ${FAM_PATH} --score ${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range ${RANGE_LIST} ${SUMSTATS}.SNP.pvalue 
+	        --extract ${RESULTS_PATH}/SURF/${TRAIT}.${ANCESTRY}.SURF.${PARTITION}.clump.clumped --keep ${SAMPLES_PATH} --out ${RESULTS_PATH}/SURF/${TRAIT}.${ANCESTRY}.SURF.${PARTITION}
 
-#TURF
-\${PLINK_PATH} --bfile \${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump \${SUMSTATS} --clump-snp-field SNP --clump-field P \
-        --extract \${TURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.\${TISSUE}.\${SLURM_ARRAY_TASK_ID}.SNPs --out \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}.clump
+	#TURF
+	${PLINK_PATH} --bfile ${EUR_1KG_BFILE} --clump-p1 1 --clump-p2 1 --clump-r2 0.2 --clump-kb 250 --clump ${SUMSTATS} --clump-snp-field SNP --clump-field P 
+	        --extract ${TURF_PATH}/1000G_phase3_master_scores_1KG-pruned_quantile_normalized_chrALL.${TISSUE}.${PARTITION}.SNPs --out ${RESULTS_PATH}/TURF/${TRAIT}.${ANCESTRY}.TURF.${PARTITION}.clump
 
-\${PLINK2_PATH} --pgen \${BED_PATH} --pvar \${BIM_PATH} --psam \${FAM_PATH} --score \${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range \${RANGE_LIST} \${SUMSTATS}.SNP.pvalue \
-        --extract \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}.clump.clumped --keep \${SAMPLES_PATH} --out \${RESULTS_PATH}/TURF/\${TRAIT}.\${ANCESTRY}.TURF.\${SLURM_ARRAY_TASK_ID}
+	${PLINK2_PATH} --pgen ${BED_PATH} --pvar ${BIM_PATH} --psam ${FAM_PATH} --score ${SUMSTATS} 1 2 3 header no-mean-imputation --q-score-range ${RANGE_LIST} ${SUMSTATS}.SNP.pvalue 
+	        --extract ${RESULTS_PATH}/TURF/${TRAIT}.${ANCESTRY}.TURF.${PARTITION}.clump.clumped --keep ${SAMPLES_PATH} --out ${RESULTS_PATH}/TURF/${TRAIT}.${ANCESTRY}.TURF.${PARTITION}
+done
 
-exit
-EOF
+touch ${RESULTS_PATH}/${TRAIT}.OK
